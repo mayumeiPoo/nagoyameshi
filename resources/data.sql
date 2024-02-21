4fb2c3530abdd99d65c5e408b6dc0a8939dc0532
mayumeiPoo <146159227+mayumeiPoo@users.noreply.github.com>
2024-02-21 23:18:37 +0900

    Add files via upload

--- resources/data.sql
+++ resources/data.sql
@@ -1,12 +1,13 @@
 --roleテーブル
 INSERT IGNORE INTO role (id, name) VALUES (1, 'ROLE_GENERAL');
-INSERT IGNORE INTO role (id, name) VALUES (2, 'ROLE_ADMIN');
+INSERT IGNORE INTO role (id, name) VALUES (2, 'ROLE_PREMIUM');
+INSERT IGNORE INTO role (id, name) VALUES (3, 'ROLE_ADMIN');
 
 
 
 --userテーブル
 INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, birthmonth, birthday, gender, email, password, role_id, enabled) VALUES (1, '侍太郎', 'サムライタロウ', '111-1111', '東京都千代田区神田練塀町300番地', '090-1234-5678', '01', '02', '男性', 'taro.samurai@example.com', '$2a$10$2JNjTwZBwo7fprL2X4sv.OEKqxnVtsVQvuXDkI8xVGix.U3W5B7CO', 1, true ); 
-INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, birthmonth, birthday, gender, email, password, role_id, enabled) VALUES (2, '侍花子', 'サムライハナコ', '111-1111', '東京都千代田区神田練塀町300番地', '090-1234-5678', '03', '02', '女性', 'hanako.samurai@example.com', '$2a$10$2JNjTwZBwo7fprL2X4sv.OEKqxnVtsVQvuXDkI8xVGix.U3W5B7CO', 2, true );
+INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, birthmonth, birthday, gender, email, password, role_id, enabled) VALUES (2, '侍花子', 'サムライハナコ', '111-1111', '東京都千代田区神田練塀町300番地', '090-1234-5678', '03', '02', '女性', 'hanako.samurai@example.com', '$2a$10$2JNjTwZBwo7fprL2X4sv.OEKqxnVtsVQvuXDkI8xVGix.U3W5B7CO', 3, true );
 INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, birthmonth, birthday, gender, email, password, role_id, enabled) VALUES (3, '侍 義勝', 'サムライ ヨシカツ', '111-1111', '東京都千代田区神田練塀町300番地', '090-1234-5678', '10', '02', '男性',  'yoshikatsu.samurai@example.com', 'password', 1, false );
 INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, birthmonth, birthday, gender, email, password, role_id, enabled) VALUES (4, '侍 幸美', 'サムライ サチミ', '111-1111', '東京都千代田区神田練塀町300番地', '090-1234-5678', '12', '2', '女性', 'sachimi.samurai@example.com', 'password', 1, false );
 INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, birthmonth, birthday, gender, email, password, role_id, enabled) VALUES (5, '侍 雅', 'サムライ ミヤビ', '111-1111', '東京都千代田区神田練塀町300番地', '090-1234-5678', '11', '23', '女性', 'miyabi.samurai@example.com', 'password', 1, false );
---
+++ resources/static/js/map.js
@@ -0,0 +1,2 @@
+<script defer src="https://maps.googleapis.com/maps/api/js?key={AIzaSyCtPm_1RFZHhvrBw95zXKoCHwbba26HJqw}&callback=initialize"></script>
+
