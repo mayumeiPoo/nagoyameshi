6171eda7c09027512f885e36768b77ec73ce87bc
mayumeiPoo <146159227+mayumeiPoo@users.noreply.github.com>
2024-02-26 17:44:35 +0900

    Delete js directory

--- js/flatpickr.js
+++
@@ -1,8 +0,0 @@
-let maxDate = new Date();
- maxDate = maxDate.setMonth(maxDate.getMonth() + 3);
- 
- flatpickr('#fromReservationDate', {
-   locale: 'ja',
-   minDate: 'today',
-   maxDate: maxDate
- });
